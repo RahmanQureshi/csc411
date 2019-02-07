7381679893f83b98f7ab9fc6652e1eb88c15f931
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-02-06 19:59:28 -0500

    complete hw3

--- hw3/q2.py
+++ hw3/q2.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_boston
+import sklearn.model_selection
 np.random.seed(0)
 
 # load boston housing prices dataset
@@ -19,7 +20,8 @@ y = boston['target']
 
 idx = np.random.permutation(range(N))
 
-#helper function
+# helper function
+# did not end up using this
 def l2(A,B):
     '''
     Input: A is a Nxd matrix
@@ -44,14 +46,34 @@ def LRLS(test_datum,x_train,y_train, tau,lam=1e-5):
            lam is the regularization parameter
     output is y_hat the prediction on test_datum
     '''
-    ## TODO
-    return None
-    ## TODO
+    N = len(x_train)
+    D = len(x_train[0])
+    deltaX = x_train-test_datum.transpose()
+    normSquares = -1*(np.sum(np.abs(deltaX)**2,axis=-1))/(2*tau**2)
+    expNormalizedNormSquares = np.exp(normSquares - max(normSquares))
+    A = np.diag(expNormalizedNormSquares/sum(expNormalizedNormSquares))
+    # Recall: dC/dw = -(Y-Xw)^T AX + lam*w^T
+    # Use a linear solver to solve this
+    AA = np.dot(np.dot(x_train.transpose(), A), x_train) + lam*np.identity(D)
+    bb = np.dot(np.dot(x_train.transpose(), A), y_train)
+    wStar = np.linalg.solve(AA, bb)
+    return np.dot(test_datum.transpose(), wStar)
+
+# computes a single loss given (x,t) by training on x_train, y_train
+def loss(x_train, y_train, x, t, tau, lam=1e-5):
+    yhat = LRLS(x, x_train, y_train, tau, lam)
+    return (0.5)*(yhat-t)**2
+
+# compute average loss (or the cost) on (x_test, t_test) by training on (x_train, y_train)
+def cost(x_train, y_train, x_test, t_test, tau, lam=1e-5):
+    N = len(x_test)
+    cost = 0
+    for i in range(0, N):
+        cost += (1./N)*loss(x_train, y_train, x_test[i], t_test[i], tau, lam)
+    return cost
 
 
-
-
-def run_validation(x,y,taus,val_frac):
+def run_validation(x,y,taus,val_frac,lam=1e-5):
     '''
     Input: x is the N x d design matrix
            y is the N x 1 targets vector    
@@ -61,14 +83,21 @@ def run_validation(x,y,taus,val_frac):
            a vector of training losses, one for each tau value
            a vector of validation losses, one for each tau value
     '''
-    ## TODO
-    return None
-    ## TODO
-
+    x_train, x_validate, y_train, y_validate = sklearn.model_selection.train_test_split(x, y, test_size=val_frac)
+    num_iter = len(taus)
+    training_cost = [0 for i in range(0, num_iter)]
+    validation_cost = [0 for i in range(0, num_iter)]
+    for i in range(0, num_iter):
+        print("Iteration: %d" % i)
+        training_cost[i] = cost(x_train, y_train, x_train, y_train, taus[i], lam)
+        validation_cost[i] = cost(x_train, y_train, x_validate, y_validate, taus[i], lam)
+    return (training_cost, validation_cost)
 
 if __name__ == "__main__":
     # In this excersice we fixed lambda (hard coded to 1e-5) and only set tau value. Feel free to play with lambda as well if you wish
-    taus = np.logspace(1.0,3,200)
+    taus = np.linspace(1,1000,300)
     train_losses, test_losses = run_validation(x,y,taus,val_frac=0.3)
-    plt.semilogx(train_losses)
-    plt.semilogx(test_losses)
+    plt.semilogx(taus, train_losses)
+    plt.semilogx(taus, test_losses)
+    plt.legend(['training loss', 'test loss'])
+    plt.show()
