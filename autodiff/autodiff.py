d516948eb786adafd55a6c09e4878444ba026cea
Abdurrahman Qureshi <rahman.a.qureshi@gmail.com>
2019-02-14 23:31:05 -0500

    do toposort

--- autodiff/autodiff.py
+++ autodiff/autodiff.py
@@ -1,4 +1,7 @@
 import numpy as np
+from collections import deque
+from collections import defaultdict
+from toposort import toposort_flatten
 
 class Node:
 
@@ -13,6 +16,10 @@ class Node:
 
     def __str__(self):
         return self.value.__str__()
+
+    def __mul__(self, other):
+
+        pass        
 
     def prop(self):
         for parent, vjp in zip(self.parents, self.vjps):
@@ -30,22 +37,42 @@ def sin(x):
 def sinsin(x):
     return sin(sin(x))
 
-def backprop(end_node):
+def sinsinsin(x):
+    return sin(sin(sin(x)))
+
+def sinsquared(x):
+    return sin(x)*sin(x)
+
+def buildGraph(end_node):
+    G = defaultdict(set)
+    visited = set()
+    queue = deque([end_node])
+    while len(queue) > 0:
+        current = queue.popleft()
+        visited.add(current)
+        if current.parents == None:
+            continue
+        for parent in current.parents:
+            G[current].add(parent)
+            if parent not in visited:
+                queue.append(parent)
+    return G
+
+def backprop(end_node, start_node):
     end_node.v = np.ones(len(end_node.value)) # deriv wrt itself is 1
-    current_node = end_node
-    while True:
-        if current_node.parents == None:
-            break
-        current_node.prop()
-        current_node = current_node.parents[0] # for now, assuming one parent (i.e. a chain)
-    return current_node.v
+    G = buildGraph(end_node)
+    for node in reversed(toposort_flatten(G)):
+        if node.parents == None:
+            continue
+        node.prop()
+    return start_node.v
 
 # returns gradient of fct evaluated at input
 def grad(fct, x):
     start_node = Node(x, None, None)
     end_node = fct(start_node)
-    return backprop(end_node)
+    return backprop(end_node, start_node)
 
 if __name__ == "__main__":
-    gradVal = grad(sinsin, [0.2])
+    gradVal = grad(sinsinsin, [0.2])
     print(gradVal)
